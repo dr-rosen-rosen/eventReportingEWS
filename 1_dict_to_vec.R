f547944fa7ab41ce1ff42932acffa4dc4ed983d0
mrosen44 <mrosen44@mrosen44s-MacBook-Pro.local>
2024-06-25 14:38:29 -0400

    more dictionary to vector store

--- 1_dict_to_vec.R
+++ 1_dict_to_vec.R
@@ -9,6 +9,9 @@ con <- DBI::dbConnect(RPostgres::Postgres(),
                       port = config$dbPort
 )
 
+##### General Inquirer
+### NOTE: probably need a better system for rolling this across system and dictionarie
+
 # create column if not exists
 insert_new_column(
   con = con, 
@@ -19,17 +22,29 @@ insert_new_column(
 )
 
 # serialize for storage
-nrc_df$gi_vec <- dict_to_vec(
-  nrc_df[,grepl("^gi_",names(nrc_df))],
+# nrc_df$gi_vec <- dict_to_vec(
+#   nrc_df[,grepl("^gi_",names(nrc_df))],
+#   serialize = TRUE)
+# # save as vector in db
+# by(nrc_df, seq_len(nrow(nrc_df)), insert_vec, con = con, sys_source = 'nrc', col_name = 'gi_vec')
+
+# serialize for storage
+asrs_df$gi_vec <- dict_to_vec(
+  asrs_df[,grepl("^gi_",names(asrs_df))],
   serialize = TRUE)
-
 # save as vector in db
+by(asrs_df, seq_len(nrow(asrs_df)), insert_vec, con = con, sys_source = 'asrs', col_name = 'gi_vec')
 
+# serialize for storage
+rail_df$gi_vec <- dict_to_vec(
+  rail_df[,grepl("^gi_",names(rail_df))],
+  serialize = TRUE)
+# save as vector in db
+by(rail_df, seq_len(nrow(rail_df)), insert_vec, con = con, sys_source = 'rail', col_name = 'gi_vec')
 
-by(nrc_df[1:2,], seq_len(nrow(nrc_df[1:2,])), insert_vec, con = con, sys_source = 'nrc', col_name = 'gi_vec')
-
-insert_vec(con = con,
-           vec = nrc_df[1,'gi_vec'],
-           )
-
-nrc_embeds <- DBI::dbExecute(conn = con, s)
+# serialize for storage
+phmsa_df$gi_vec <- dict_to_vec(
+  phmsa_df[,grepl("^gi_",names(phmsa_df))],
+  serialize = TRUE)
+# save as vector in db
+by(phmsa_df, seq_len(nrow(phmsa_df)), insert_vec, con = con, sys_source = 'phmsa', col_name = 'gi_vec')
